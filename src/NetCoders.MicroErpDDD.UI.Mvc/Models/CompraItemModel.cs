b6d4005b1bfe284f502d70ee9fc022d6c553a515
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2015-04-23 22:14:04 -0300

    Adicionando Template Jquery, configurando o Web.config e editando o model de CompraItem.

--- src/NetCoders.MicroErpDDD.UI.Mvc/Models/CompraItemModel.cs
+++ src/NetCoders.MicroErpDDD.UI.Mvc/Models/CompraItemModel.cs
@@ -1,12 +1,18 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace NetCoders.MicroErpDDD.UI.Mvc.Models
 {
     public class CompraItemModel
     {
+        [Required]
         public int IdProduto { get; set; }
 
+        public string ProdutoNome { get; set; }
+
+        [Required]
         public int Quantidade { get; set; }
 
+        [Required]
         public decimal Preco { get; set; }
     }
 }
